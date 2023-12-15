834c94e321338911e5e533f28fc95c0392ab1db1
sanaa <sanaaennaji93@gmail.com>
2023-12-15 09:53:41 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="head">
+    <h1>ToDo List</h1>
+    </div>
+    <form action="index.php" method="get">
+        <input type="text" name="task" >
+        <button type="submit" name="submit" class="btn" >add</button>
+    </form>
+
+
+
+</body>
+</html>
